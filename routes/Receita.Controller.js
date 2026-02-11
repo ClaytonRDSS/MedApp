89da48c0b0574d6daeebb7369ab0ac18edfe2473
Clayton Roberto Dos Santos <clayton.silveira87@hotmail.com>
2026-02-11 16:28:07 -0300

    Crud_receita

--- routes/Receita.Controller.js
+++ routes/Receita.Controller.js
@@ -1,5 +1,63 @@
 import { express } from 'node:express';
+import ReceitaService from '..//services/ReceitaService.js';
+
+
 
 let router = express.Router();
+
+router.get('/receita', async(req, res) => {
+    try {
+        const receita = await ReceitaService.getAllReceita();
+        res.send(receita);
+    } catch(error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
+
+router.get('/receita/:id', async(req, res) => {
+    const {id} = req.params;
+    try {
+        const receita = await ReceitaService.getReceita(id);
+        res.send(receita);
+    } catch(error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
+
+router.post('/postReceita', async(req, res) => {
+    const {data, consultaId, medicamento, dosagemMedicamento, instrucoes} = req.body;
+    try {
+        const receita = await ReceitaService.saveReceita({data, consultaId, medicamento, dosagemMedicamento, instrucoes});
+        res.send(receita);
+    } catch(error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
+
+router.put('/receita/:id', async(req, res) => {
+    const {id} = req.params;
+    const {data, consultaId, medicamento, dosagemMedicamento, instrucoes} = req.body;
+    try {
+        const receita = await ReceitaService.updateReceita(id, {data, consultaId, medicamento, dosagemMedicamento, instrucoes});
+        res.send(receita);
+    } catch(error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
+
+router.delete('/receita/:id', async(req, res) => {
+    const {id} = req.params;
+    try {
+        const receita = await ReceitaService.deleteReceita(id);
+        res.send(receita);
+    } catch(error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
 
 export default router();
--- services/ReceitaService.js
+++ services/ReceitaService.js
@@ -1,5 +1,30 @@
-const receitaService = {
+import ReceitaRepository from "../repositories/ReceitaRepository";
 
+const getAllReceita = async () => {
+    return ReceitaRepository.getAllReceita(); 
+}
+
+const getReceita = async (id) => {
+    return ReceitaRepository.getReceita(id); 
+}
+
+const saveReceita = async ({data, consultaId, medicamento, dosagemMedicamento, instrucoes}) => {
+    return ReceitaRepository.saveReceita({data, consultaId, medicamento, dosagemMedicamento, instrucoes}); 
+}
+
+const updateReceita = async (id, {data, consultaId, medicamento, dosagemMedicamento, instrucoes}) => {
+    return ReceitaRepository.updateReceita(id, {data, consultaId, medicamento, dosagemMedicamento, instrucoes}); 
+}
+
+const deleteReceita = async (id) => {
+    return ReceitaRepository.deleteReceita(id); 
+}
+const receitaService = {
+    getAllReceita,
+    getReceita,
+    saveReceita,
+    updateReceita,
+    deleteReceita
 }
 
 export default receitaService;
