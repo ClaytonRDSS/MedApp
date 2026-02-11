936f6e3a2074f22c7e6aef60accdfbfabd3ad7c7
Clayton Roberto Dos Santos <clayton.silveira87@hotmail.com>
2026-02-11 11:17:12 -0300

    criando-rotas-dos-controlles

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,23 @@
 import { express } from 'node:express';
 
+//importando os Controlles para o arquivo de rodas para costruir as rotas 
+import consultaController from './ConsultaController.js';
+import medicoController from './MedicoController.js';
+import pacienteController from './PacienteController.js';
+import receitaController from './Receita.Controller.js';
+
+
 let router = express.Router();
 
 router.get("/", function(req, res) {
     console.log("Oi!");
     res.status(200).jason({message: "Oi!"});
 });
+
+router.use('/', consultaController);
+router.use('/', medicoController);
+router.use('/', pacienteController);
+router.use('/', receitaController);
+
 
 export default router;
